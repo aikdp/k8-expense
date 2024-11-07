c106e1a878de2a851d57f7a0954fa487fa66b327
aikdp <kdprasad028@gmail.com>
2024-11-07 11:45:01 +0530

    k8 expense practice

--- all-eksctl.sh
+++ all-eksctl.sh
@@ -41,4 +41,9 @@ sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
 
 
+#kubens 
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
 #last do AWS configure
